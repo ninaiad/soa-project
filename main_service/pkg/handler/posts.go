e2bb755e3ee37d67bfaaf9c672d028059b5bc1e8
Nina Gulevich <nina.m.gulevich@gmail.com>
2024-03-28 16:36:17 +0300

    get and delete don't take request body: enter query parameters

--- main_service/pkg/handler/posts.go
+++ main_service/pkg/handler/posts.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"soa/posts"
+	"strconv"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -18,19 +19,9 @@ type postIdMsg struct {
 	PostId int `json:"post_id"`
 }
 
-type updatePostInput struct {
-	postIdMsg
-	postTextMsg
-}
-
 type postResponse struct {
 	Text        string `json:"text"`
 	TimeUpdated string `json:"time_updated"`
-}
-
-type postsByPageInput struct {
-	PageNum  int `json:"page_num"`
-	PageSize int `json:"page_size"`
 }
 
 type postsByPageOutput struct {
@@ -69,13 +60,25 @@ func (h *Handler) updatePost(c *gin.Context) {
 		return
 	}
 
-	var input updatePostInput
+	postIdS, ok := c.GetQuery("id")
+	if !ok {
+		newErrorResponse(c, http.StatusBadRequest, "no id parameter for updated post")
+		return
+	}
+
+	postId, err := strconv.Atoi(postIdS)
+	if err != nil {
+		newErrorResponse(c, http.StatusBadRequest, "id parameter not a number")
+		return
+	}
+
+	var input postTextMsg
 	if err := c.BindJSON(&input); err != nil {
 		newErrorResponse(c, http.StatusBadRequest, "invalid input body")
 		return
 	}
 
-	_, err = h.services.PostsServerClient.UpdatePost(context.Background(), &posts.UpdateRequest{AuthorId: int32(userId), PostId: int32(input.PostId), Text: input.Text})
+	_, err = h.services.PostsServerClient.UpdatePost(context.Background(), &posts.UpdateRequest{AuthorId: int32(userId), PostId: int32(postId), Text: input.Text})
 	if err != nil {
 		newErrorResponse(c, http.StatusInternalServerError, err.Error())
 		return
@@ -92,13 +95,19 @@ func (h *Handler) deletePost(c *gin.Context) {
 		return
 	}
 
-	var input postIdMsg
-	if err := c.BindJSON(&input); err != nil {
-		newErrorResponse(c, http.StatusBadRequest, "invalid input body")
+	postIdS, ok := c.GetQuery("id")
+	if !ok {
+		newErrorResponse(c, http.StatusBadRequest, "no id parameter for deleted post")
 		return
 	}
 
-	_, err = h.services.PostsServerClient.DeletePost(context.Background(), &posts.PostIdRequest{AuthorId: int32(userId), PostId: int32(input.PostId)})
+	postId, err := strconv.Atoi(postIdS)
+	if err != nil {
+		newErrorResponse(c, http.StatusBadRequest, "id parameter not a number")
+		return
+	}
+
+	_, err = h.services.PostsServerClient.DeletePost(context.Background(), &posts.PostIdRequest{AuthorId: int32(userId), PostId: int32(postId)})
 	if err != nil {
 		newErrorResponse(c, http.StatusInternalServerError, err.Error())
 		return
@@ -115,13 +124,19 @@ func (h *Handler) getPost(c *gin.Context) {
 		return
 	}
 
-	var input postIdMsg
-	if err := c.BindJSON(&input); err != nil {
-		newErrorResponse(c, http.StatusBadRequest, "invalid input body")
+	postIdS, ok := c.GetQuery("id")
+	if !ok {
+		newErrorResponse(c, http.StatusBadRequest, "no id parameter for post")
 		return
 	}
 
-	post, err := h.services.PostsServerClient.GetPost(context.Background(), &posts.PostIdRequest{AuthorId: int32(userId), PostId: int32(input.PostId)})
+	postId, err := strconv.Atoi(postIdS)
+	if err != nil {
+		newErrorResponse(c, http.StatusBadRequest, "id parameter not a number")
+		return
+	}
+
+	post, err := h.services.PostsServerClient.GetPost(context.Background(), &posts.PostIdRequest{AuthorId: int32(userId), PostId: int32(postId)})
 	if err != nil {
 		newErrorResponse(c, http.StatusInternalServerError, err.Error())
 		return
@@ -138,13 +153,31 @@ func (h *Handler) getPageOfPosts(c *gin.Context) {
 		return
 	}
 
-	var input postsByPageInput
-	if err := c.BindJSON(&input); err != nil {
-		newErrorResponse(c, http.StatusBadRequest, "invalid input body")
+	pageNumS, ok := c.GetQuery("page_num")
+	if !ok {
+		newErrorResponse(c, http.StatusBadRequest, "no page_num parameter")
 		return
 	}
 
-	posts, err := h.services.PostsServerClient.GetPageOfPosts(context.Background(), &posts.GetPageOfPostsRequest{AuthorId: int32(userId), PageNum: int32(input.PageNum), PageSize: int32(input.PageSize)})
+	pageNum, err := strconv.Atoi(pageNumS)
+	if err != nil {
+		newErrorResponse(c, http.StatusBadRequest, "page_num parameter not a number")
+		return
+	}
+
+	pageSizeS, ok := c.GetQuery("page_size")
+	if !ok {
+		newErrorResponse(c, http.StatusBadRequest, "no page_size parameter")
+		return
+	}
+
+	pageSize, err := strconv.Atoi(pageSizeS)
+	if err != nil {
+		newErrorResponse(c, http.StatusBadRequest, "page_size parameter not a number")
+		return
+	}
+
+	posts, err := h.services.PostsServerClient.GetPageOfPosts(context.Background(), &posts.GetPageOfPostsRequest{AuthorId: int32(userId), PageNum: int32(pageNum), PageSize: int32(pageSize)})
 	if err != nil {
 		newErrorResponse(c, http.StatusInternalServerError, err.Error())
 		return
@@ -155,6 +188,6 @@ func (h *Handler) getPageOfPosts(c *gin.Context) {
 		postsData[i] = postResponse{Text: (*posts).Posts[i].Text, TimeUpdated: (*posts).Posts[i].TimeUpdated.AsTime().Format(time.RFC3339)}
 	}
 
-	log.Println("successful getPostsbyPage request")
+	log.Println("successful getPageOfPosts request")
 	c.JSON(http.StatusOK, postsByPageOutput{PageNum: int(posts.PageNum), PageSize: int(posts.PageSize), Posts: postsData})
 }
